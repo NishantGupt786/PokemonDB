10198b98c04833ad92cb817e9eb2218cd442b2f4
Your Name <you@example.com>
2023-06-03 17:07:05 +0530

    fourth commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 const ejs = require("ejs");
 const Pokedex = require("pokedex");
+const {getPokemon,getAllPokemon,getAllPokemonNames} = require('pkmonjs');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -16,16 +17,27 @@ app.get("/", (req,res)=>{
     res.render("index");
 });
 
-app.post("/", (req,res)=>{
+app.post("/",async (req,res)=>{
     const pmon = req.body.pokemon;
+    const poke = await getPokemon(pmon);
     const pokemonData = pokedex.pokemon(pmon);
     console.log(pokemonData);
-    res.render("pokemon", {pokemonData:pokemonData});
+    console.log(poke);
+    res.render("pokemon", {pokemonData:pokemonData, poke: poke});
 });
 
-app.get("/pokemonDB", (req, res)=>{
-    const pokemonarr = pokedex.pokemon();
+app.get("/pokemonDB",async (req, res)=>{
+    const pokemonarr=await getAllPokemon();
+    //console.log(pokemonarr);
+    //res.send(pokemonarr);
     res.render("pokemonDB", {pokemonarr: pokemonarr});
+})
+
+app.get("/pokemon", async (req, res)=>{
+    const name = req.body.name;
+    console.log(name);
+    const poke =await getPokemon(name);
+    res.render("pokemon", {poke: poke});
 })
 
 app.listen(3000, function() {
